127667c1e0e1ee339a88c7ce0f79b935e5895d3e
HuseinHQ <mhusein463@gmail.com>
2023-10-26 01:08:56 +0700

    Fix seeders

--- seeders/20231008082212-create-first-user.js
+++ seeders/20231008082212-create-first-user.js
@@ -7,12 +7,12 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     const user = require("../data/user.json").map((el) => {
       el.createdAt = el.updatedAt = new Date();
+      el.password = hashPassword(el.password);
       return el;
     });
 
     const userProfile = require("../data/profile.json").map((el) => {
       el.createdAt = el.updatedAt = new Date();
-      el.password = hashPassword(el.password);
       return el;
     });
 
