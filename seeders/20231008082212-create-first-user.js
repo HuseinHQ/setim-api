b533f61c1a054fb5fb6c29a21dd80187db0da279
HuseinHQ <mhusein463@gmail.com>
2023-10-26 01:06:00 +0700

    Fix seeders

--- seeders/20231008082212-create-first-user.js
+++ seeders/20231008082212-create-first-user.js
@@ -1,26 +1,27 @@
-'use strict';
+"use strict";
+
+const { hashPassword } = require("../helpers/bcrypt");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    const user = require('../data/user.json')
-      .map(el => {
-        el.createdAt = el.updatedAt = new Date();
-        return el;
-      })
+    const user = require("../data/user.json").map((el) => {
+      el.createdAt = el.updatedAt = new Date();
+      return el;
+    });
 
-    const userProfile = require('../data/profile.json')
-      .map(el => {
-        el.createdAt = el.updatedAt = new Date();
-        return el;
-      })
+    const userProfile = require("../data/profile.json").map((el) => {
+      el.createdAt = el.updatedAt = new Date();
+      el.password = hashPassword(el.password);
+      return el;
+    });
 
-    await queryInterface.bulkInsert('Users', user);
-    await queryInterface.bulkInsert('Profiles', userProfile)
+    await queryInterface.bulkInsert("Users", user);
+    await queryInterface.bulkInsert("Profiles", userProfile);
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Profiles', null)
-    await queryInterface.bulkDelete('Users', null)
-  }
+    await queryInterface.bulkDelete("Profiles", null);
+    await queryInterface.bulkDelete("Users", null);
+  },
 };
