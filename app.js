c04cdfd958b13fba32f1ae28c6c8451d3e9b6701
HuseinHQ <mhusein463@gmail.com>
2023-10-26 01:00:21 +0700

    Deployment init

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const app = express();
 const PORT = process.env.PORT || 3000;
 const cors = require("cors");
---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: "app1",
+      script: "./app.js",
+      env: {
+        NODE_ENV: "production",
+        SECRET_KEY: "aplikasisangatrahasia060302@#$",
+        "Client-ID": "5ztrd2zlqq9njnotja2x0e049zu0e0",
+        Authorization: "Bearer s4sndh60xembbpktvbaae3g5du9uqx",
+        MIDTRANS_SERVER_KEY: "SB-Mid-server-hecguXN4p-vUIHMJzRgoTQvB",
+      },
+    },
+  ],
+};
